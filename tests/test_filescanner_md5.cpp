a1527f67f74934184836d7e3ed7f0363e1189898
ilyaytrewq <tixonov.ilay.2000@mail.ru>
2025-09-20 19:41:34 +0300

    deb

--- tests/test_filescanner_md5.cpp
+++ tests/test_filescanner_md5.cpp
@@ -2,28 +2,38 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
-#include "VirusScanner.hpp" 
+#include <chrono>
+#include <thread>
+#include "VirusScanner.hpp"
 
 TEST(FileScannerMD5Test, KnownContent) {
     using namespace std::filesystem;
     auto tmpdir = temp_directory_path() / ("vs_test_" + std::to_string(std::chrono::steady_clock::now().time_since_epoch().count()));
     create_directories(tmpdir);
 
-
     path f = tmpdir / "hello.txt";
-    std::ofstream ofs(f, std::ios::binary);
-    ofs << "hello";
-    ofs.close();
+    {
+        std::ofstream ofs(f, std::ios::binary);
+        ofs << "hello";
+        ofs.close(); 
+    }
 
-
-    std::ifstream ifs(f, std::ios::binary);
-    FileScanner scanner(std::move(ifs), f, 1024);
-    scanner.calculateFileHash();
-    std::string hash = scanner.getFileHashString();
-
+    std::string hash;
+    {
+        std::ifstream ifs(f, std::ios::binary);
+        FileScanner scanner(std::move(ifs), f, 1024);
+        scanner.calculateFileHash();
+        hash = scanner.getFileHashString();
+    }
 
     EXPECT_EQ(hash, "5d41402abc4b2a76b9719d911017c592"); // md5("hello")
 
-
-    std::filesystem::remove_all(tmpdir);
+    std::error_code ec;
+    for (int i = 0; i < 5; ++i) {
+        ec.clear();
+        remove_all(tmpdir, ec);
+        if (!ec) break;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    
 }
