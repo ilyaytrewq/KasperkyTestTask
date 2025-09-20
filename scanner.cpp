206988ccea7420785decacdf4cfeb27d956d8293
ilyaytrewq <tixonov.ilay.2000@mail.ru>
2025-09-20 15:46:58 +0300

    delete debug vars

--- scanner.cpp
+++ scanner.cpp
@@ -3,78 +3,16 @@
 #include <filesystem>
 #include <stdexcept>
 #include <iostream>
-#include <getopt.h>
-
-#include <stdio.h>
-
-void parseArgs(int argc, char *argv[], std::filesystem::path &dirPath, std::filesystem::path &basePath, std::filesystem::path &logPath, size_t &inputBufferSize, size_t &outputBufferSize, size_t &threadCount) {
-    const char * shortOptions = "b:l:p:i:o:t:";
-
-    const struct option longOptions[] = {
-        { "base", required_argument, 0, 'b' },
-        { "log", required_argument, 0, 'l' },
-        { "path", required_argument, 0, 'p' },
-        { "ibuf", required_argument, 0, 'i' },
-        { "obuf", required_argument, 0, 'o' },
-        { "threads", required_argument, 0, 't' },
-        { NULL, 0, NULL, 0 }
-    };
-
-    int opt;
-    while ((opt = getopt_long(argc, argv, shortOptions, longOptions, NULL)) != -1) {
-        switch (opt)
-        {
-        case 'b':
-            basePath = optarg;
-            break;
-        case 'l':
-            logPath = optarg;
-            break;
-        case 'p':
-            dirPath = optarg;
-            break;
-        case 'i':
-            if (optarg)
-                inputBufferSize = std::stoul(optarg); 
-            break;
-        case 'o':
-            if (optarg)
-                outputBufferSize = std::stoul(optarg);
-            break;
-        case 't':
-            if (optarg)
-                threadCount = std::stoul(optarg);
-            break;
-        case '?':
-            throw std::runtime_error("Unknown option");
-        default:
-            break;
-        }
-    }    
-
-    if (dirPath.empty()) {
-        throw std::invalid_argument("Missing required argument: --path");
-    }
-
-    if (basePath.empty()) {
-        throw std::invalid_argument("Missing required argument: --base");
-    }
-
-    if(logPath.empty()) {
-        throw std::invalid_argument("Missing required argument: --log");
-    }
-
-}
 
 int main(int argc, char* argv[]) {
     TimerGuard timer("Virus scanner execution time: ", std::cout);    
 
-    std::filesystem::path dirPath = "/home/ilyaytrewq/Documents/", basePath = "/home/ilyaytrewq/Documents/projects/KasperkyTestTask/base.csv", logPath = "/home/ilyaytrewq/Documents/projects/KasperkyTestTask/report.log";
+    std::filesystem::path dirPath = "", basePath = "", logPath = "";
     
     size_t inputBufferSize = 16 * 1024 - 100, outputBufferSize = 16 * 1024, threadCount = 0;
 
     try {
-        parseArgs(argc, argv, dirPath, basePath, logPath, inputBufferSize, outputBufferSize, threadCount);
+        ParseArgs(argc, argv, dirPath, basePath, logPath, inputBufferSize, outputBufferSize, threadCount);
     } catch (const std::exception& e) {
         std::cerr << "Error parsing arguments: " << e.what() << std::endl;
         std::cerr << "Usage: " << argv[0] << " --base <file> --log <file> --path <directory>" << std::endl;
